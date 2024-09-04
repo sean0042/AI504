ae33f7f9b51d64cdf77885c7ef77462aa1332996
Sunjun Kweon <sean0042@kaist.ac.kr>
2024-09-04 14:51:59 +0900

    change SYSTEM_PROMPT

--- chains.py
+++ chains.py
@@ -1,75 +1,80 @@
-
-from langchain_core.messages import ChatMessage,HumanMessage,AIMessage
-from langchain_community.document_loaders import WebBaseLoader
-from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate,MessagesPlaceholder
 from langchain.chains import create_history_aware_retriever, create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
-from langchain_core.tracers.context import collect_runs
-from streamlit_feedback import streamlit_feedback
 from langchain.retrievers import BM25Retriever, EnsembleRetriever 
 import streamlit as st
-from langsmith import Client
-from langsmith import traceable
 
 
-system_prompt = (
-    "You are a teaching assistant for the KAIST AI504 course 'Programming for AI,' which focuses on learning NumPy and PyTorch."
-    "You may use the following retrieved context to answer the student's question if relevant."
-    "The retrieved context consists of course materials, practice materials, class audio transcriptions, and the syllabus."
-    "For example, '01_numpy.pdf' refers to the first week's lecture materials, and '01_numpy.ipynb' refers to the first week's practice materials."
-    "However, if the context is not necessary to answer the question, you may choose not to use it."
-    "If you do use the context, you must cite the source (noted at the beginning of each context) in your response.\nContext :"
+SYSTEM_PROMPT = (
+    "You are a teaching assistant for the KAIST AI504 course, 'Programming for AI,' which primarily focuses on learning PyTorch."
+    "Your duty is to assist students by answering any course-related questions."
+    "When responding to student questions, you may refer to the retrieved contexts."
+    "The retrieved contexts consist of text excerpts from various course materials, practice materials, lecture transcriptions, and the syllabus."
+    "On top of each context, there is a tag (e.g., '01_numpy.pdf') that indicates its source."
+    "For example, '01_numpy.pdf' refers to the lecture material for the first week, and '01_numpy.ipynb' refers to the practice materials from the same week."
+    "You may choose to answer without using the context if it is unnecessary."
+    "However, if you do use the context, make sure to cite the source (noted at the beginning of each context) in your response.\nContext:"
 )
 
 
 def get_vector_store():
-    vector_store= FAISS.load_local(
-        "./faiss_db/20240902", 
-        embeddings=OpenAIEmbeddings(model="text-embedding-3-small"), 
-        allow_dangerous_deserialization=True)
+    # Load a local FAISS vector store
+
+    vector_store = FAISS.load_local(
+        "./faiss_db/", 
+        embeddings = OpenAIEmbeddings(model = "text-embedding-3-small"), 
+        allow_dangerous_deserialization = True)
+    
     return vector_store
 
 
 
 def get_retreiver_chain(vector_store):
-  llm=ChatOpenAI(model = "gpt-4o-mini", temperature = 0)
+    # This function takes a vector store as input and returns a retriever chain that is aware of conversation history.
+    # It combines FAISS and BM25 retrievers for more effective information retrieval.
 
-  faiss_retriever = vector_store.as_retriever(
-    search_kwargs={"k": 2},
-  )
-  bm25_retriever = BM25Retriever.from_documents(
-     st.session_state.docs
-  )
-  bm25_retriever.k = 2
+    llm = ChatOpenAI(model = "gpt-4o-mini", temperature = 0)
 
-  ensemble_retriever = EnsembleRetriever(
-      retrievers=[bm25_retriever, faiss_retriever],
-  )
-  
-  prompt = ChatPromptTemplate.from_messages([
-      MessagesPlaceholder(variable_name="chat_history"),
-      ("user","{input}"),
-      ("user","Given the above conversation, generate a search query to look up in order to get information relevant to the conversation")
-  ])
-  history_retriver_chain = create_history_aware_retriever(llm,ensemble_retriever,prompt)
-  return history_retriver_chain
+    faiss_retriever = vector_store.as_retriever(
+       search_kwargs={"k": 2},
+    )
+    bm25_retriever = BM25Retriever.from_documents(
+       st.session_state.docs
+    )
+    bm25_retriever.k = 2
+
+    ensemble_retriever = EnsembleRetriever(
+        retrievers = [bm25_retriever, faiss_retriever],
+    )
+
+    prompt = ChatPromptTemplate.from_messages([
+        MessagesPlaceholder(variable_name = "chat_history"),
+        ("user","{input}"),
+        ("user","Given the above conversation, generate a search query to look up in order to get information relevant to the conversation")
+    ])
+    history_retriver_chain = create_history_aware_retriever(llm, ensemble_retriever, prompt)
+
+    return history_retriver_chain
 
 
 
 
 def get_conversational_rag(history_retriever_chain):
-  llm=ChatOpenAI(model = "gpt-4o-mini", temperature = 0)
-  answer_prompt=ChatPromptTemplate.from_messages([
-      ("system",system_prompt+"\n\n{context}"),
-      MessagesPlaceholder(variable_name="chat_history"),
+  # Create end-to-end RAG chain
+  llm = ChatOpenAI(model = "gpt-4o-mini", temperature = 0)
+
+  answer_prompt = ChatPromptTemplate.from_messages([
+      ("system",SYSTEM_PROMPT+"\n\n{context}"),
+      MessagesPlaceholder(variable_name = "chat_history"),
       ("user","{input}")
   ])
 
   document_chain = create_stuff_documents_chain(llm,answer_prompt)
-  conversational_retrieval_chain = create_retrieval_chain(history_retriever_chain,document_chain)
+
+  conversational_retrieval_chain = create_retrieval_chain(history_retriever_chain, document_chain)
+
   return conversational_retrieval_chain
 
