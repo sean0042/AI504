96e4f33f0e3655b0c3ca1605f36f409bc84c3990
Sunjun Kweon <sean0042@kaist.ac.kr>
2024-09-09 22:17:50 +0900

    change langsmith rep

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import os
 os.environ["LANGCHAIN_API_KEY"] = st.secrets["LANGCHAIN_API_KEY"]
 os.environ["LANGCHAIN_TRACING_V2"] = 'true'
 os.environ["LANGCHAIN_ENDPOINT"] = "https://api.smith.langchain.com"
-os.environ["LANGCHAIN_PROJECT"] = "AI504_test"
+os.environ["LANGCHAIN_PROJECT"] = "AI504"
 
 def main():
     if "student_id" not in st.session_state:
