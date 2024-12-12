2550b9af990d4f48750ecd556abd972d82209d5f
Sunjun Kweon <sean0042@kaist.ac.kr>
2024-12-12 13:46:26 -0800

    add 12/12 class

--- chains.py
+++ chains.py
@@ -29,7 +29,7 @@ SYSTEM_PROMPT = (
     "12th week, 11/19 (Tuesday), BERT & GPT (https://youtu.be/_DCqSooULV8?si=Ljm0Mrt_Z5uu46tM), 11/21 (Thursday), BERT & GPT Practice Session(https://youtu.be/2mXeDREBGnw?si=uyvgF3umaP0U9N-n)\n"
     "13th week, 11/26 (Tuesday), Project 2: Language Model (https://youtu.be/sR51xTljdf8?si=Zab_AhYza-G-6LNQ), 11/28 (Thursday), No Class\n"
     "14th week, 12/3 (Tuesday), Deep Diffusion Probabilistic Model (https://youtu.be/4hZj3mXj6PI?si=JCuYnjeR3WopQ4my), 12/5 (Thursday), Deep Diffusion Probabilistic Model Practice Session(https://youtu.be/NvCYnUz_0pM?si=JRAJkAAGlvRUIYbF)\n"
-    "15th week, 12/10 (Tuesday), Image-Text Multi-modal Learning(https://youtu.be/fs2fPSQs3kA?si=WnoYxlX-3Cg-JDf3), 12/12 (Thursday), Image-Text Multi-modal Learning Practice Session\n"
+    "15th week, 12/10 (Tuesday), Image-Text Multi-modal Learning(https://youtu.be/fs2fPSQs3kA?si=WnoYxlX-3Cg-JDf3), 12/12 (Thursday), Image-Text Multi-modal Learning Practice Session(https://youtu.be/jOnCRs1WCd4?si=hEeV-LpKoNB0AXz1)\n"
     "16th week, 12/17 (Tuesday), Project 3: Vision-Language Model, 12/19 (Thursday), No Class\n\n"
     "Your duty is to assist students by answering any course-related questions.\n"
     "If the question is related to projects, tell them to check the KLMS announcements.\n"
